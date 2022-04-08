0d602a7bf40af3792ecbe96addf1c5b7533dd3fb
muhammadjunaidkhalid <mjk22071998@gmail.com>
2022-04-08 12:05:51 +0500

    Image created

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'avs.ui'
-#
-# Created by: PyQt5 UI code generator 5.14.1
-#
-# WARNING! All changes made in this file will be lost!
-
 import os
 
 from PyQt5 import QtCore, QtGui, QtWidgets
