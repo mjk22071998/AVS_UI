3e850f981cf939966f09431ade898328bc528b40
muhammadjunaidkhalid <mjk22071998@gmail.com>
2022-04-08 11:14:17 +0500

    file decoded

--- main.py
+++ main.py
@@ -6,11 +6,19 @@
 #
 # WARNING! All changes made in this file will be lost!
 
+import os
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QFileDialog
 
 
 class Ui_Form(object):
+
+
+    def __init__(self):
+        self.selectFile = None
+        self.fileName=""
+
     def setupUi(self, Form):
         Form.setObjectName("Form")
         Form.resize(723, 542)
@@ -54,16 +62,27 @@ class Ui_Form(object):
 
     def selectImageClicker(self):
         print("Select Image clicked")
+        self.openFileNameDialog()
 
     def decodeImageClicker(self):
-        print("Select Image clicked")
+        print("Decode Image clicked")
+        os.system("python3 apktoimage.py "+self.fileName+" .")
 
     def displayImageClicker(self):
         print("Select Image clicked")
 
+    def openFileNameDialog(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        self.fileName, path = QFileDialog.getOpenFileName(None, "QFileDialog.getOpenFileName()", "",
+                                                     "APK Files (*.apk)", options=options)
+        if self.fileName:
+            print(self.fileName)
+
 
 if __name__ == "__main__":
     import sys
+
     app = QtWidgets.QApplication(sys.argv)
     Form = QtWidgets.QWidget()
     ui = Ui_Form()
